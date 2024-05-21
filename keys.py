875799713e1c035a7718ad2c697314bc366bba87
Soumava Das <112095521+Kazuto16K@users.noreply.github.com>
2024-05-22 02:45:23 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,5 @@
-account_sid = 'ACf28036e8422dd45348d22b71ce97e6a3'
-auth_token = '16368a84ea085a051177cb698102c263'
+account_sid = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+auth_token = 'xxxxxxxxxxxxxxxxxxxxxxx'
 
-twilio_number = '+16365381415'
-target_number = '+918420357221'
+twilio_number = 'xxxxxxxxxxxxxx'
+target_number = '+91xxxxxxxxxx'
